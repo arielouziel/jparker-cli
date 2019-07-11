9ba30068c6211c6ecd770173800d5af85fa357cb
Ariel Ouziel <ariel.ouziel3@docapost.fr>
2019-07-11 16:51:32 +0200

    fix api test

--- src/test/java/com/aouziel/jparker/client/api/ParkingLotControllerApiTest.java
+++ src/test/java/com/aouziel/jparker/client/api/ParkingLotControllerApiTest.java
@@ -14,93 +14,43 @@
 package com.aouziel.jparker.client.api;
 
 import com.aouziel.jparker.client.invoker.ApiException;
-import com.aouziel.jparker.client.model.ParkingLot;
-import com.aouziel.jparker.client.model.ParkingSlot;
-import com.aouziel.jparker.client.model.ParkingTicket;
-import org.junit.Ignore;
-import org.junit.Test;
+import com.aouziel.jparker.client.model.*;
+import org.junit.*;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * API tests for ParkingLotControllerApi
  */
-@Ignore
 public class ParkingLotControllerApiTest {
 
-    private final ParkingLotControllerApi api = new ParkingLotControllerApi();
+    private static final ParkingLotControllerApi api = new ParkingLotControllerApi();
+    private static ParkingLot testParkingLot;
 
-    
-    /**
-     * Create a new parking lot
-     *
-     * 
-     *
-     * @throws ApiException
-     *          if the Api call fails
-     */
-    @Test
-    public void createParkingLotTest() throws ApiException {
-        ParkingLot parkingLot = null;
-        ParkingLot response = api.createParkingLot(parkingLot);
+    @Before
+    public void setup() throws ApiException {
+        testParkingLot = new ParkingLot().name("Parking Lot under test")
+                .pricingPolicy(new HourRatePricingPolicy()
+                        .hourPrice(100)
+                        .currencyCode("EUR")
+                        .pricingPolicyType("HOURRATEPRICINGPOLICY"));
+        testParkingLot = api.createParkingLot(testParkingLot);
+        assertThat(testParkingLot.getId()).isNotNull();
 
-        // TODO: test validations
+        for (int i = 0; i < 2; i++) { // create 10 slots
+            ParkingSlot parkingSlot = new ParkingSlot()
+                    .type(ParkingSlot.TypeEnum.SEDAN);
+            ParkingSlot slot = api.createParkingSlot(testParkingLot.getId(), parkingSlot);
+        }
     }
-    
-    /**
-     * Create a new slot in a parking lot
-     *
-     * 
-     *
-     * @throws ApiException
-     *          if the Api call fails
-     */
-    @Test
-    public void createParkingSlotTest() throws ApiException {
-        Long lotId = null;
-        ParkingSlot slot = null;
-        ParkingSlot response = api.createParkingSlot(lotId, slot);
 
-        // TODO: test validations
-    }
-    
-    /**
-     * Delete a parking
-     *
-     * 
-     *
-     * @throws ApiException
-     *          if the Api call fails
-     */
-    @Test
-    public void deleteParkingLotTest() throws ApiException {
-        Long lotId = null;
-        api.deleteParkingLot(lotId);
 
-        // TODO: test validations
-    }
-    
-    /**
-     * Put a car in a any free parking slot
-     *
-     * 
-     *
-     * @throws ApiException
-     *          if the Api call fails
-     */
-    @Test
-    public void enterParkingLotTest() throws ApiException {
-        Long lotId = null;
-        String carPowerType = null;
-        ParkingTicket response = api.enterParkingLot(lotId, carPowerType);
-
-        // TODO: test validations
-    }
-    
     /**
      * View a list of all parking lots
      *
-     * 
+     *
      *
      * @throws ApiException
      *          if the Api call fails
@@ -108,76 +58,128 @@ public class ParkingLotControllerApiTest {
     @Test
     public void getAllParkingLotsTest() throws ApiException {
         List<ParkingLot> response = api.getAllParkingLots();
-
-        // TODO: test validations
+        assertThat(response).isNotNull().isNotEmpty();
     }
-    
+
     /**
-     * Get a parking lot by id
+     * Create a new parking lot
      *
-     * 
+     *
      *
      * @throws ApiException
      *          if the Api call fails
      */
     @Test
-    public void getParkingLotByIdTest() throws ApiException {
-        Long lotId = null;
-        ParkingLot response = api.getParkingLotById(lotId);
-
-        // TODO: test validations
+    public void createParkingLotTest() throws ApiException {
+        ParkingLot parkingLot = new ParkingLot().name("Test Parking Lot")
+                .pricingPolicy(new HourRatePricingPolicy()
+                        .hourPrice(100)
+                        .currencyCode("EUR")
+                        .pricingPolicyType("HOURRATEPRICINGPOLICY"));
+        ParkingLot response = api.createParkingLot(parkingLot);
+        assertThat(response.getId()).isNotNull();
     }
-    
+
     /**
-     * Remove car from parking lot and bill the customer
+     * Delete a parking
      *
-     * 
+     *
      *
      * @throws ApiException
      *          if the Api call fails
      */
     @Test
-    public void leaveParkingLotTest() throws ApiException {
-        Long lotId = null;
-        String ticketNumber = null;
-        ParkingTicket response = api.leaveParkingLot(lotId, ticketNumber);
+    public void deleteParkingLotTest() throws ApiException {
+        ParkingLot parkingLot = new ParkingLot().name("Test To Remove Parking Lot")
+                .pricingPolicy(new HourRatePricingPolicy()
+                        .hourPrice(100)
+                        .currencyCode("EUR")
+                        .pricingPolicyType("HOURRATEPRICINGPOLICY"));
+        parkingLot = api.createParkingLot(parkingLot);
+        assertThat(parkingLot.getId()).isNotNull();
 
-        // TODO: test validations
+        api.deleteParkingLot(parkingLot.getId());
     }
-    
+
     /**
-     * Get a list of free slots in a parking lot
+     * Create a new slot in a parking lot
      *
-     * 
+     *
      *
      * @throws ApiException
      *          if the Api call fails
      */
     @Test
-    public void getParkingSlotsTest() throws ApiException {
-        Long lotId = null;
-        String parkingSlotStatus = null;
-        String parkingSlotType = null;
-        List<ParkingSlot> response = api.getParkingSlots(lotId, parkingSlotStatus, parkingSlotType);
-
-        // TODO: test validations
+    public void createParkingSlotTest() throws ApiException {
+        ParkingSlot response = api.createParkingSlot(testParkingLot.getId(), new ParkingSlot().type(ParkingSlot.TypeEnum.SEDAN));
+        assertThat(response.getId()).isNotNull();
     }
-    
+
     /**
      * Remove a slot from a parking lot
      *
-     * 
+     *
      *
      * @throws ApiException
      *          if the Api call fails
      */
     @Test
     public void removeParkingSlotTest() throws ApiException {
-        Long lotId = null;
-        Long slotId = null;
-        api.removeParkingSlot(lotId, slotId);
+        ParkingSlot slot = api.createParkingSlot(testParkingLot.getId(), new ParkingSlot().type(ParkingSlot.TypeEnum.SEDAN));
+        assertThat(slot.getId()).isNotNull();
 
-        // TODO: test validations
+        api.removeParkingSlot(testParkingLot.getId(), slot.getId());
     }
-    
+
+    /**
+     * Get a parking lot by id
+     *
+     *
+     *
+     * @throws ApiException
+     *          if the Api call fails
+     */
+    @Test
+    public void getParkingLotByIdTest() throws ApiException {
+        api.getParkingLotById(testParkingLot.getId());
+    }
+
+    /**
+     * Remove car from parking lot and bill the customer
+     *
+     *
+     *
+     * @throws ApiException
+     *          if the Api call fails
+     */
+    @Test
+    public void enterAndleaveParkingLotTest() throws ApiException {
+        ParkingTicket ticket = api.enterParkingLot(testParkingLot.getId(), ParkingTicket.CarPowerTypeEnum.SEDAN.toString());
+        assertThat(ticket.getNumber()).isNotNull();
+
+        ticket = api.leaveParkingLot(testParkingLot.getId(), ticket.getNumber());
+        assertThat(ticket.getPrice()).isGreaterThan(0);
+    }
+
+    /**
+     * Get a list of free slots in a parking lot
+     *
+     *
+     *
+     * @throws ApiException
+     *          if the Api call fails
+     */
+    @Test
+    public void getParkingSlotsTest() throws ApiException {
+        Long lotId = testParkingLot.getId();
+        String parkingSlotStatus = "free";
+        String parkingSlotType = "sedan";
+        List<ParkingSlot> response = api.getParkingSlots(lotId, parkingSlotStatus, parkingSlotType);
+        assertThat(response).isNotEmpty().isNotNull();
+    }
+
+    @After
+    public void tearDown() throws ApiException, InterruptedException {
+        api.deleteParkingLot(testParkingLot.getId());
+    }
 }
